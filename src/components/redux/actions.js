996f89688a510a5ba7a59e1dee28af29e474bba0
MateuszCharysz <charyszek@interia.pl>
2023-06-28 23:13:36 +0200

    Update actions.js

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -1,3 +1,30 @@
-import { createAction, nanoid } from "@reduxjs/toolkit";
+import { createAction, nanoid } from '@reduxjs/toolkit';
 
-const addContact = createAction('contacts/addContact', ()=>{}) // można dodać funkcję tworzącą id
+const addContact = createAction(
+  'contacts/addContact',
+  ({ nameinput, numberinput }) => {
+    return {
+      payload: {
+        id: nanoid(),
+        name: nameinput,
+        number: numberinput,
+      },
+    };
+  },
+); // można dodać funkcję tworzącą id
+
+const filterContact = createAction('contacts/filterContact'); //TODO czy to powinno być do filter czy do contacts?
+
+const deleteContact = createAction('contacts/deleteContact')
+
+const setName = createAction('name/setName')
+
+const setNumber = createAction('number/setNumber')
+
+const resetName = createAction('name/resetName')
+
+const resetNumber = createAction('number/resetNumber')
+
+const actions = { addContact, filterContact, deleteContact, setName, setNumber, resetName, resetNumber };
+
+export default actions;
